c5fe8d0578335f184b2d9135bec8416f6d2cffda
Victor0123 <victor.rossini01@hotmail.com>
2024-11-01 14:29:47 -0300

    Add endpoints de tarefas e seus respectivos test

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,7 +2,7 @@ from dataclasses import asdict
 
 from sqlalchemy import select
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
 def test_create_user(session, mock_db_time):
@@ -20,6 +20,24 @@ def test_create_user(session, mock_db_time):
         'username': 'alice',
         'password': 'secret',
         'email': 'teste@test',
+        'todos': [],
         'created_at': time,
         'updated_at': time,
     }
+
+
+def test_create_todo(session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
